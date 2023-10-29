43321f0548d9e390bb1abaf3b7ae675cbefe1d40
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-29 13:00:23 +0300

    Implementing the showContent logic

--- frontend/components/Questions/Question.tsx
+++ frontend/components/Questions/Question.tsx
@@ -6,11 +6,13 @@ const Question = ({ data, showContent }: Props) => {
 	return (
 		<div className='bg-zinc-200 m-2 p-4 rounded-sm'>
 			<div className=''>{data.title}</div>
-			<div className=''>
-				{data.content.length > 50
-					? `${data.content.substring(0, 50)}...`
-					: data.content}
-			</div>
+			{showContent && (
+				<div className=''>
+					{data.content.length > 50
+						? `${data.content.substring(0, 50)}...`
+						: data.content}
+				</div>
+			)}
 			<div className=''>{`Asked by ${
 				data.userName
 			} on ${data.created.toLocaleDateString()} ${data.created.toLocaleTimeString()}`}</div>

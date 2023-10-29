3639109616791f26927f4fb4a6834fb26eba2e18
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-29 13:15:00 +0300

    Set the logic to retreive the questions asynchronously

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,8 +1,23 @@
-import { getUnansweredQuestions } from '@/QuestionsData'
+import { QuestionData, getUnansweredQuestions } from '@/QuestionsData'
 import QuestionList from '@/components/Questions/QuestionList'
 import { Button } from '@/components/ui/button'
+import { useEffect, useState } from 'react'
 
 export default function Home() {
+	const [questions, setQuestions] = useState<QuestionData[]>([])
+	const [questionsLoading, setQuestionsLoading] = useState(true)
+
+	useEffect(() => {
+		const doGetUnansweredQuestions = async () => {
+			const unansweredQuestions = await getUnansweredQuestions()
+
+			setQuestions(unansweredQuestions)
+			setQuestionsLoading(false)
+		}
+
+		doGetUnansweredQuestions()
+	}, [])
+
 	return (
 		<main className='h-[90vh] w-screen pt-5 px-5 flex flex-col items-center gap-10'>
 			<div className='flex flex-col gap-2'>
@@ -10,7 +25,7 @@ export default function Home() {
 				<Button>Ask A Question</Button>
 			</div>
 			<div className=''>
-				{/* <QuestionList data={getUnansweredQuestions()} /> */}
+				<QuestionList data={questions} />
 			</div>
 		</main>
 	)

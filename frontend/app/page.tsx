91f2cb12643c3b90ba88e73533b373ece5a62579
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-29 12:54:15 +0300

    Rendering out the questions

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,10 +1,17 @@
+import { getUnansweredQuestions, questions } from '@/QuestionsData'
+import QuestionList from '@/components/Questions/QuestionList'
 import { Button } from '@/components/ui/button'
 
 export default function Home() {
 	return (
-		<main className='h-[90vh] w-screen p-10'>
-			<h2>Unanswered Questions</h2>
-			<Button>Ask A Question</Button>
+		<main className='h-[90vh] w-screen pt-5 px-5 flex flex-col items-center gap-10'>
+			<div className='flex flex-col gap-2'>
+				<h2>Unanswered Questions</h2>
+				<Button>Ask A Question</Button>
+			</div>
+			<div className=''>
+				<QuestionList data={questions} />
+			</div>
 		</main>
 	)
 }

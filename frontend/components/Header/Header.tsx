3197b36042334ddd26c43379e38bef8e14a5e88b
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-29 11:59:18 +0300

    Added some styles for the nav in the header

--- frontend/components/Header/Header.tsx
+++ frontend/components/Header/Header.tsx
@@ -1,7 +1,11 @@
 type Props = {}
 
 const Header = (props: Props) => {
-	return <nav className='h-[10vh] w-screen shadow-md'>Header</nav>
+	return (
+		<nav className='h-[10vh] w-screen shadow-md flex flex-row justify-between'>
+			Header
+		</nav>
+	)
 }
 
 export default Header

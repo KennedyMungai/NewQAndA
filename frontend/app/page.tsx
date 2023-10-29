ac71ef69e222e7a52e48d40506124f45943ca4a8
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-29 12:18:16 +0300

    Added some text and a button to the index page of the component page

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,10 @@
+import { Button } from '@/components/ui/button'
+
 export default function Home() {
-	return <main className='h-[90vh] w-screen'></main>
+	return (
+		<main className='h-[90vh] w-screen p-10'>
+			<h2>Unanswered Questions</h2>
+			<Button>Ask A Question</Button>
+		</main>
+	)
 }

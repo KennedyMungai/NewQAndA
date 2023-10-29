0756489bf1a4739fb12ef5a44d6e5e6b256c6db2
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-29 14:37:10 +0300

    Creaed the userProfile component

---
+++ frontend/components/Header/UserProfile.tsx
@@ -0,0 +1,25 @@
+import { Avatar, AvatarImage, AvatarFallback } from '../ui/avatar'
+import React from 'react'
+import { Button } from '../ui/button'
+
+type Props = {
+	isLoggedIn: boolean
+	authHandler: () => void
+}
+
+const UserProfile = ({ isLoggedIn, authHandler }: Props) => {
+	return (
+		<div>
+			{isLoggedIn ? (
+				<Avatar>
+					<AvatarImage src={''} alt='Profile Image' />
+					<AvatarFallback>PI</AvatarFallback>
+				</Avatar>
+			) : (
+				<Button onClick={authHandler}>Log In / Sign Up</Button>
+			)}
+		</div>
+	)
+}
+
+export default UserProfile

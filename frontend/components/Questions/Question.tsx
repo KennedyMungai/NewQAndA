9d1734266edef3a19cb82ff86c434ea8d69484e5
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-29 12:56:57 +0300

    Added the questions content

--- frontend/components/Questions/Question.tsx
+++ frontend/components/Questions/Question.tsx
@@ -6,6 +6,11 @@ const Question = ({ data }: Props) => {
 	return (
 		<div>
 			<div className=''>{data.title}</div>
+			<div className=''>
+				{data.content.length > 50
+					? `${data.content.substring(0, 50)}...`
+					: data.content}
+			</div>
 			<div className=''>{`Asked by ${
 				data.userName
 			} on ${data.created.toLocaleDateString()} ${data.created.toLocaleTimeString()}`}</div>

4a21bad05a8a1f65e57ea86b8ba202899222320c
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-31 14:52:00 +0300

    Created the AnswerList component

---
+++ frontend/components/Answers/AnswerList.tsx
@@ -0,0 +1,22 @@
+import { AnswerData } from '@/QuestionsData'
+import React from 'react'
+
+type Props = {
+	data: AnswerData[]
+}
+
+const AnswerList = ({ data }: Props) => {
+	return (
+		<ul className='list-none mt-10'>
+			{data.map((answer) => {
+				return (
+					<li key={answer.answerId}>
+						<Answer data={answer} />
+					</li>
+				)
+			})}
+		</ul>
+	)
+}
+
+export default AnswerList

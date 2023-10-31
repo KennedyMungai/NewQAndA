40a07b36e660ed5a3146b85137969daa05baa11f
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-31 14:30:48 +0300

    Deleted some logic from the question page

--- frontend/app/question/page.tsx
+++ frontend/app/question/page.tsx
@@ -1,12 +1,7 @@
 'use client'
-import { QuestionData } from '@/QuestionsData'
-import React, { useState } from 'react'
-
 type Props = {}
 
 const QuestionPage = (props: Props) => {
-	const [question, setQuestion] = useState<QuestionData | null>(null)
-
 	return <div>QuestionPage</div>
 }
 

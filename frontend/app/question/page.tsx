6aa578b0de28ca090be7b8761a4867681d6d4d47
Kennedy Mungai <kennedymungaifmab@gmail.com>
2023-10-31 14:28:37 +0300

    Created the question state for the component

--- frontend/app/question/page.tsx
+++ frontend/app/question/page.tsx
@@ -1,8 +1,12 @@
-import React from 'react'
+'use client'
+import { QuestionData } from '@/QuestionsData'
+import React, { useState } from 'react'
 
 type Props = {}
 
 const QuestionPage = (props: Props) => {
+	const [question, setQuestion] = useState<QuestionData | null>(null)
+
 	return <div>QuestionPage</div>
 }
 
